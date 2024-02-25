7130c5f9002c515ff003e5f8975c9a8b7e59a5c9
vandilsonbrito <vandilson25@outlook.com>
2024-02-25 16:24:08 -0300

    Create GlobalProvider component

---
+++ src/web/components/GlobalProvider/GlobalProvider.jsx
@@ -0,0 +1,37 @@
+import { useContext, createContext, useState } from "react";
+import PropTypes from 'prop-types';
+
+const GlobalContext = createContext();
+
+export function GlobalProvider({ children }) {
+    const [numberOfMistoQuente, setNumberOfMistoQuente] = useState(0);
+    const [numberOfXTudo, setNumberOfXTudo] = useState(0);
+    const [numberOfSandMortadela, setNumberOfSandMortadela] = useState(0);
+    const [numberOfXSalada, setNumberOfXSalada] = useState(0);
+    const [numberOfBauru, setNumberOfBauru] = useState(0);
+    const [numberOfSandPernil, setNumberOfSandPernil] = useState(0);
+    const [numberOfAmericano, setNumberOfAmericano] = useState(0);
+    const [numberOfFrangoQueijo, setNumberOfFrangoQueijo] = useState(0);
+    const [numberOfXBacon, setNumberOfXBacon] = useState(0);
+    const [numberOfXCatupiry, setNumberOfXCatupiry] = useState(0);
+    const [numberOfSandAtum, setNumberOfSandAtum] = useState(0);
+    const [numberOfSandSalame, setNumberOfSandSalame] = useState(0);
+    const [numberOfMarmitex, setNumberOfMarmitex] = useState(0);
+    const [isAdded, setIsAdded ] = useState(false);
+    const [isCartOpen, setIsCartOpen ] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <GlobalContext.Provider value={{ numberOfMistoQuente, setNumberOfMistoQuente, numberOfXTudo, setNumberOfXTudo, numberOfSandMortadela, setNumberOfSandMortadela, numberOfXSalada, setNumberOfXSalada, numberOfBauru, setNumberOfBauru, numberOfSandPernil, setNumberOfSandPernil, numberOfAmericano, setNumberOfAmericano, numberOfFrangoQueijo, setNumberOfFrangoQueijo, numberOfXBacon, setNumberOfXBacon, numberOfXCatupiry, setNumberOfXCatupiry, numberOfSandAtum, setNumberOfSandAtum, numberOfSandSalame, setNumberOfSandSalame, numberOfMarmitex, setNumberOfMarmitex, isAdded, setIsAdded, isCartOpen, setIsCartOpen, isMenuOpen, setIsMenuOpen}}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
+
+export function useGlobal() {
+    return useContext(GlobalContext);
+}
+
+GlobalProvider.propTypes = {
+    children: PropTypes.node
+};
