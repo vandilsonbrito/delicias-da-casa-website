045990386af7e01b844b029c55082c6dd3044591
vandilsonbrito <vandilson25@outlook.com>
2024-03-05 15:03:39 -0300

    Style: Updates styles

--- src/web/components/DishesMenu/DishesMenu.jsx
+++ src/web/components/DishesMenu/DishesMenu.jsx
@@ -1,5 +1,4 @@
 import marmitexImg from '../../assets/images/marmitex.webp';
-import { MdAddCircle } from "react-icons/md";
 import { RiCloseCircleFill } from "react-icons/ri";
 import { useGlobal } from '../GlobalProvider/GlobalProvider';
 
@@ -9,27 +8,24 @@ export default function DishesMenu() {
     const { numberOfMarmitex, setNumberOfMarmitex } = useGlobal();
 
   return (
-    <div className="w-full h-full lg:px-28 bg-white border-b-[1px] border-slate-200"> 
-        <h3 className="text-[2rem] text-left mt-16 mb-10 ml-12">Pratos</h3>
+    <div className="w-full h-full pb-20 lg:px-28 bg-white border-b-[1px] border-slate-200"> 
+        <h3 className="text-[2rem] text-left mt-16 mb-7 ml-12">Pratos</h3>
         
         <div className="w-full h-full flex flex-wrap justify-center gap-2">
             <div className="container-dish ">
-                <img className='w-[220px] h-[170px] rounded-3xl' src={marmitexImg} alt="" />
+                <button onClick={() => setNumberOfMarmitex(numberOfMarmitex + 1)}>
+                    <img className='w-[220px] h-[170px] rounded-3xl' src={marmitexImg} alt="" />
+                </button>
                 <p className="font-bold text-xl text-center mt-2 tracking-wider">Marmitex - R$ 15,00</p>
                 <div className="w-[90%] mt-2 flex items-center justify-center">
                     <p className="text-sm text-center">Feijão, Arroz, Carne e Salada</p>
                 </div>
             </div>
             <div className="absolute">
-                    <div className="relative right-[105px]">
+                    <div className="relative top-[36px] right-[105px]">
                         <div className="menu-quantity">{numberOfMarmitex}</div>
                     </div>
-                    <div className="relative -top-[40px] -right-[115px] flex flex-col">
-                        <button 
-                        className="btn-cardapio" 
-                        onClick={() => {setNumberOfMarmitex(numberOfMarmitex + 1)}}>
-                            <MdAddCircle className='text-[1.7rem]'/>
-                        </button>
+                    <div className="relative top-[5px] -right-[110px] flex flex-col">
                         <button 
                         className="btn-cardapio mt-1"
                         onClick={() => setNumberOfMarmitex(numberOfMarmitex === 0 ? 0 : (numberOfMarmitex - 1))}>
