92cb067e9ab51c6861c18c3ac1e562afb2d171e1
vandilsonbrito <vandilson25@outlook.com>
2024-03-12 16:07:27 -0300

    Feat: Checking isOrderButtonActive then hidden or not 'order  button'

--- src/web/components/Header/Header.jsx
+++ src/web/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo-delicias-da-casa.png';
+import PropTypes from 'prop-types';
 
-export default function Header() {
+
+export default function Header({ className, isOrderButtonActive }) {
   return (
-    <div className="w-full h-24 fixed z-50">
+    <div className={className}>
         <header className="w-full h-24 bg-black flex justify-between items-center py-5 px-5 md:px-20 text-white">
             <img className='h-20 md:h-24' src={logoImg} alt="Logo Padaria do Baiano" />
             <nav className="w-[40%] justify-between text-[1.2rem]  hidden lg:flex">
@@ -20,10 +22,15 @@ export default function Header() {
                     <button>Contato</button>
                 </Link>
             </nav>
-            <Link to='CartCheckout'>
-                <button className='bg-green-600 px-4 py-2 md:px-5 md:py-3 rounded-2xl font-bold text-base active:scale-[.98]'>Fazer Pedido</button>
+            <Link to='CartCheckout' className={`${isOrderButtonActive ? 'visible' : 'hidden'}`}>
+                <button className='bg-green-600 px-4 py-2 md:px-5 md:py-3 rounded-2xl font-bold text-base active:scale-[.98] tracking-wider'>Fazer Pedido</button>
             </Link>
         </header>
     </div>
   )
+}
+
+Header.propTypes = {
+    className: PropTypes.string,
+    isOrderButtonActive: PropTypes.bool,
 }
