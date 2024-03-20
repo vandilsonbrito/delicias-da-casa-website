35ce90fb27a5c9960bd3840aede06a4ede076679
vandilsonbrito <vandilson25@outlook.com>
2024-03-20 00:47:06 -0300

    Feat: delete snacks and dishes components and add menu component

--- src/web/pages/Home/Home.jsx
+++ src/web/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import Banner from "../../components/Banner/Banner";
 import Header from "../../components/Header/Header";
-import Snacks from "../../components/SnacksMenu/SnacksMenu";
-import Dishes from "../../components/DishesMenu/DishesMenu";
+import Menu from "../../components/Menu/Menu";
 import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
@@ -9,8 +8,7 @@ export default function Home() {
     <>
       <Header className={'w-full h-24 fixed z-50'} isOrderButtonActive= {true}/>
       <Banner/>
-      <Snacks/>
-      <Dishes/>
+      <Menu/>
       <Footer/>
     </>
   )
