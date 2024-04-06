38ccb36de6e2d3d6fb53d4f90735c78d11c1c3c9
vandilsonbrito <vandilson25@outlook.com>
2024-04-06 13:30:09 -0300

    Feat: add isnavactive global variable

--- src/web/components/GlobalProvider/GlobalProvider.jsx
+++ src/web/components/GlobalProvider/GlobalProvider.jsx
@@ -18,10 +18,10 @@ export function GlobalProvider({ children }) {
     const [numberOfSandSalame, setNumberOfSandSalame] = useState(0);
     const [numberOfMarmitex, setNumberOfMarmitex] = useState(0);
     const [isOrderButtonActive, setisOrderButtonActive ] = useState(false);
-
+    const [isNavActive, setisNavActive ] = useState(true);
 
     return (
-        <GlobalContext.Provider value={{ numberOfMistoQuente, setNumberOfMistoQuente, numberOfXTudo, setNumberOfXTudo, numberOfSandMortadela, setNumberOfSandMortadela, numberOfXSalada, setNumberOfXSalada, numberOfBauru, setNumberOfBauru, numberOfSandPernil, setNumberOfSandPernil, numberOfAmericano, setNumberOfAmericano, numberOfFrangoQueijo, setNumberOfFrangoQueijo, numberOfXBacon, setNumberOfXBacon, numberOfXCatupiry, setNumberOfXCatupiry, numberOfSandAtum, setNumberOfSandAtum, numberOfSandSalame, setNumberOfSandSalame, numberOfMarmitex, setNumberOfMarmitex, isOrderButtonActive, setisOrderButtonActive }}>
+        <GlobalContext.Provider value={{ numberOfMistoQuente, setNumberOfMistoQuente, numberOfXTudo, setNumberOfXTudo, numberOfSandMortadela, setNumberOfSandMortadela, numberOfXSalada, setNumberOfXSalada, numberOfBauru, setNumberOfBauru, numberOfSandPernil, setNumberOfSandPernil, numberOfAmericano, setNumberOfAmericano, numberOfFrangoQueijo, setNumberOfFrangoQueijo, numberOfXBacon, setNumberOfXBacon, numberOfXCatupiry, setNumberOfXCatupiry, numberOfSandAtum, setNumberOfSandAtum, numberOfSandSalame, setNumberOfSandSalame, numberOfMarmitex, setNumberOfMarmitex, isOrderButtonActive, setisOrderButtonActive, isNavActive, setisNavActive }}>
             {children}
         </GlobalContext.Provider>
     );
